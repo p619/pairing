93009f5578a1336f62f0502c40f547d31725b679
p619 <p.619@hotmail.com>
2016-11-15 00:08:47 +0800

    Initial Commit

---
+++ 1115pairingTests/_115pairingTests.swift
@@ -0,0 +1,36 @@
+//
+//  _115pairingTests.swift
+//  1115pairingTests
+//
+//  Created by heroshi on 2016/11/15.
+//  Copyright © 2016年 heroshi. All rights reserved.
+//
+
+import XCTest
+@testable import _115pairing
+
+class _115pairingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
